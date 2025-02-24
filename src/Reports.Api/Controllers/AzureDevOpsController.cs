c40553da6ec0b37d8d6692c1e714672a267959e4
Marcelo Norberto <norbertosouza.marcelo@outlook.com>
2025-02-24 20:36:39 -0300

    feat: Crate api project

---
+++ src/Reports.Api/Controllers/AzureDevOpsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Reports.Api.Controllers;
+
+public class AzureDevOpsController : ControllerBase
+{
+    public async Task<IActionResult> ListAllProjectsAsync()
+    {
+        var url = "https://dev.azure.com/MNS-Tech/_apis/projects?api-version=5.0";
+
+        using (HttpClient client = new HttpClient())
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            request.Headers.Add("Authorization", "G3rF6XTzpxUBK0hk1toWJO82ZYKYsBv1icT8Svdpb4yOQNO4l6jtJQQJ99BBACAAAAAAAAAAAAASAZDO47sb");
+
+            string resposta = await client.GetStringAsync(url);
+            Console.WriteLine(resposta);
+        }
+        return Ok();
+    }
+}
